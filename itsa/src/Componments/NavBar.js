46b7ecdc5fb9e503cef7f5d1ed3875b7004321d9
Jefferywojo98 <Jefferywojo1998@gmail.com>
2023-11-11 11:59:54 -0600

    changing some minor details

--- itsa/src/Componments/NavBar.js
+++ itsa/src/Componments/NavBar.js
@@ -4,14 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavBar({ currentPage, handlePageChange }) {
   return (
-    <div class="Navbar text-center">
-    <h1 class='mainTitle'>Jeffery Portfolio</h1>
+    <div class="Navbar">
+      <div>
+          <h1 class='mainTitle'>EICC.ITSA Logo</h1>
+      </div>
+      <div class="text-center">
       <li className="nav-item text-center">
         <a href="#home"onClick={() => handlePageChange('Home')} className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}> Home</a>
       </li>
       <li className="nav-item text-center">
         <a href="#leadership" onClick={() => handlePageChange('Leadership')}className={currentPage === 'Leadership' ? 'nav-link active' : 'nav-link'}> Leadership</a>
       </li>
+      </div>
     </div>
   );
 }
